e6daaa052085c8d05ec44035335d175b87fff414
Steven Stern <steve@sterndata.com>
2014-06-21 19:05:37 -0500

    fix autop

--- shortcodes.php
+++ shortcodes.php
@@ -1,14 +1,14 @@
 <?php
-//move wpautop filter to AFTER shortcode is processed
-remove_filter( 'the_content', 'wpautop' );
-add_filter( 'the_content', 'wpautop' , 99);
-add_filter( 'the_content', 'shortcode_unautop', 100);
-
 /* 
  * left, right, and center are for divs on three column pages
  */
 
 function shortcode_wagw_cols ($atts,$content) {
+    //move wpautop filter to AFTER shortcode is processed
+    remove_filter( 'the_content', 'wpautop' );
+    add_filter( 'the_content', 'wpautop' , 99);
+    add_filter( 'the_content', 'shortcode_unautop', 100);
+
     $a = shortcode_atts( array(
         'type' => 'center',
     ), $atts );
