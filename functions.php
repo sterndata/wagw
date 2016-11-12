e0c240d5193b33d5adcc6370ff799e281f932753
Steven Stern <webmaster@sterndata.com>
2016-11-12 10:51:52 -0600

    width to 1100px

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) ) {
-	$content_width = 640; /* pixels */
+	$content_width = 1100; /* pixels */
 }
 
 if ( ! function_exists( 'wagw_setup' ) ) :
