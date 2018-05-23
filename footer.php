2e5d85ad443fc2be25ea58a8378dbab4d6c7b29b
Steven Stern <steve@sterndata.com>
2018-05-23 12:51:12 -0500

    privacy policy added to footer

--- footer.php
+++ footer.php
@@ -11,7 +11,13 @@
 	</div><!-- #content -->
 <div id="colophon-container">
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info"><a href="mailto:info@whatagreatwebsite.net">info@whatagreatwebsite.net</a> | <a href="tel:7739167684">773.916.7684</a><br />&copy; <?php echo date( 'Y' );?> What A Great Website!, LLC
+		<div class="site-info"><a href="mailto:info@whatagreatwebsite.net">info@whatagreatwebsite.net</a> | <a href="tel:7739167684">773.916.7684</a><br />&copy; <?php echo date( 'Y' );?> What A Great Website!, LLC<br>
+	                        <?php
+							if ( function_exists( 'the_privacy_policy_link' ) ) {
+								the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
+							}
+						?>
+
 		</div><!-- .site-info -->
 <?php get_sidebar( 'footer' ); ?>
 	</footer><!-- #colophon -->
